43ec1204d5a1725b267b0a3e0659ccd9237ec5c7
ebukanas <edvinas.bukanas@gmail.com>
2022-05-24 10:14:32 +0300

    .

--- main.js
+++ main.js
@@ -6,13 +6,28 @@ var show = document.querySelector('.show');
 var body = document.querySelector('body')
 
 var backgroundGradient = document.querySelector('.backgroundGradient')
-
+var closeMenu = document.getElementById('close-menu')
 
 // when you click to expand the menu icon
 menuBtn.addEventListener('click', function() {
     menu.classList.toggle('hide');
     backgroundGradient.classList.toggle('show');
+    
+    closeMenu.classList.toggle('hide');
+    menuBtn.classList.toggle('hide');
 })
+
+closeMenu.addEventListener('click', function() {
+    var menuDropdown = document.querySelector('.menu_items')
+    menuDropdown.classList.toggle('hide');
+    backgroundGradient.classList.toggle('show');
+
+    if (menuBtn.classList.contains('hide')) {
+        menuBtn.classList.remove('hide');
+        closeMenu.classList.add('hide');
+    }
+})
+
 
 // features button dropdown
 var featuresDropdown = document.querySelector('.features')
